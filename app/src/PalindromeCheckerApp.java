beb461652dea484090de86c32a0a00e9410c09f9
cp4269-cloud <cp4269@srmist.edu.in>
2026-02-25 09:25:23 +0530

    [name] Added [usecasename]

---
+++ app/src/PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+// version 1.0
+//author Abe
+//useCase 1: Welcome page
+public class Palindrone CheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker App Management System")
+    }
+}
